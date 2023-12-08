1f1854a5393a3d2ce5695a2aafa1a87aecb5b1f2
Yana <yastepankoa@gmail.com>
2023-12-08 22:27:45 +0300

    fix

--- lab2/test_convolution3d.py
+++ lab2/test_convolution3d.py
@@ -1,31 +1,33 @@
+import unittest
 import torch
-import pytest
 from Convolution3D import cv3d
 import torch.nn as nn
 
-def test_cv3d_1():
-    tensor = torch.rand(8, 5, 6, 6)
+class TestConvolution3D(unittest.TestCase):
 
-    Convolution3D = cv3d(in_channels=8, out_channels=4, kernel_size=3, stride=1, padding=0, dilation=1, groups=4, bias=True, padding_mode='zeros')
-    result, kernel_size, bias = Convolution3D(tensor)
-    torchFunction = nn.Conv3d(in_channels=8, out_channels=4, kernel_size=3, stride=1, padding=0, dilation=1, groups=4, bias=True, padding_mode='zeros')
-    torchFunction.weight.data = torch.tensor(kernel_size)
-    torchFunction.bias.data = torch.tensor(bias)
+    def test_cv3d_1(self):
+        tensor = torch.rand(8, 5, 6, 6)
 
-def test_cv3d_2():
-    tensor = torch.rand(4, 5, 5, 5)
+        Convolution3D = cv3d(in_channels=8, out_channels=4, kernel_size=3, stride=1, padding=0, dilation=1, groups=4, bias=True, padding_mode='zeros')
+        result, kernel_size, bias = Convolution3D(tensor)
+        torchFunction = nn.Conv3d(in_channels=8, out_channels=4, kernel_size=3, stride=1, padding=0, dilation=1, groups=4, bias=True, padding_mode='zeros')
+        torchFunction.weight.data = torch.tensor(kernel_size)
+        torchFunction.bias.data = torch.tensor(bias)
 
-    Convolution3D = cv3d(in_channels=4, out_channels=2, kernel_size=3, stride=2, padding=2, dilation=1, groups=2, bias=True, padding_mode='reflect')
-    result, kernel_size, bias = Convolution3D(tensor)
-    torchFunction = nn.Conv3d(in_channels=4, out_channels=2, kernel_size=3, stride=2, padding=2, dilation=1, groups=2, bias=True, padding_mode='reflect')
-    torchFunction.weight.data = torch.tensor(kernel_size)
-    torchFunction.bias.data = torch.tensor(bias)
+    def test_cv3d_2(self):
+        tensor = torch.rand(4, 5, 5, 5)
 
-def test_cv3d_3():
-    tensor = torch.rand(2, 2, 2, 2)
+        Convolution3D = cv3d(in_channels=4, out_channels=2, kernel_size=3, stride=2, padding=2, dilation=1, groups=2, bias=True, padding_mode='reflect')
+        result, kernel_size, bias = Convolution3D(tensor)
+        torchFunction = nn.Conv3d(in_channels=4, out_channels=2, kernel_size=3, stride=2, padding=2, dilation=1, groups=2, bias=True, padding_mode='reflect')
+        torchFunction.weight.data = torch.tensor(kernel_size)
+        torchFunction.bias.data = torch.tensor(bias)
 
-    Convolution3D = cv3d(in_channels=2, out_channels=2, kernel_size=2, stride=2, padding=0, dilation=1, groups=2, bias=True, padding_mode='zeros')
-    result, kernel_size, bias = Convolution3D(tensor)
-    torchFunction = nn.Conv3d(in_channels=2, out_channels=2, kernel_size=2, stride=2, padding=0, dilation=1, groups=2, bias=True, padding_mode='zeros')
-    torchFunction.weight.data = torch.tensor(kernel_size)
-    torchFunction.bias.data = torch.tensor(bias)
+    def test_cv3d_3(self):
+        tensor = torch.rand(2, 2, 2, 2)
+
+        Convolution3D = cv3d(in_channels=2, out_channels=2, kernel_size=2, stride=2, padding=0, dilation=1, groups=2, bias=True, padding_mode='zeros')
+        result, kernel_size, bias = Convolution3D(tensor)
+        torchFunction = nn.Conv3d(in_channels=2, out_channels=2, kernel_size=2, stride=2, padding=0, dilation=1, groups=2, bias=True, padding_mode='zeros')
+        torchFunction.weight.data = torch.tensor(kernel_size)
+        torchFunction.bias.data = torch.tensor(bias)
